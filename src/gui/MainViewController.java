d2d7774f2e27f3b9199d806e51d7c9708a85280b
Edir Herison <edir_h@hotmail.com>
2022-09-13 16:22:26 -0300

    Servico do Departamento

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.servicies.DepartmentService;
 
 public class MainViewController implements Initializable {
 	
@@ -31,7 +32,7 @@ public class MainViewController implements Initializable {
 	}
     @FXML
 	public void onMenuItemDepartment() {
-    	loadView("/gui/DepartmentList.fxml");
+    	loadView2("/gui/DepartmentList.fxml");
 	}
 	@FXML
 	public void onMenuItemAbout() {
@@ -54,6 +55,28 @@ public class MainViewController implements Initializable {
 		mainVbox.getChildren().clear();
 		mainVbox.getChildren().add(mainMenu);
 		mainVbox.getChildren().addAll(newVbox.getChildren());
+		
+		}
+		catch(IOException e) {
+			Alerts.showAlerts("IO Exception", " Erro Loading view",e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private synchronized void loadView2(String absoluteName) {
+		try {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+		VBox newVbox = loader.load();
+		
+		Scene mainScene = Main.getMainScene();
+		VBox mainVbox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+		Node mainMenu = mainVbox.getChildren().get(0);
+		mainVbox.getChildren().clear();
+		mainVbox.getChildren().add(mainMenu);
+		mainVbox.getChildren().addAll(newVbox.getChildren());
+		
+		DepartmentListController controller = loader.getController();
+		controller.setDepartmentService(new DepartmentService());
+		controller.updateTableView();
 		
 		}
 		catch(IOException e) {

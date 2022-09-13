e5856f5d3e8c4bdcfd93d6a07aa2c56e7e7b1a7b
Edir Herison <edir_h@hotmail.com>
2022-09-13 12:59:10 -0300

    Designe do menu

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,9 +13,12 @@ public class Main extends Application {
 	public void start(Stage primarySatge) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
+			ScrollPane scrollpane = loader.load();
 			
-			Scene mainScene = new Scene(parent);
+			scrollpane.setFitToHeight(true);
+			scrollpane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollpane);
 			primarySatge.setScene(mainScene);
 			primarySatge.setTitle("Primeiro Projeto com interface grafica e banco");
 			primarySatge.show();

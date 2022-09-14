2552e20252df3215848090f97532f20aacac4d8b
Edir Herison <edir_h@hotmail.com>
2022-09-14 09:46:28 -0300

    tela do ormulario de cadastro de dapartamento

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.servicies.DepartmentService;
@@ -36,8 +45,9 @@ public class DepartmentListController implements Initializable{
 	private ObservableList<Department> obsList;
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("Apertou o botao 'NOVO'");
+	public void onBtNovoAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
@@ -68,6 +78,24 @@ public class DepartmentListController implements Initializable{
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void createDialogForm (String absoluteName, Stage parentSatge) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialoStage = new Stage();
+			dialoStage.setTitle("Cadastro de novo Departamento");
+			dialoStage.setScene(new Scene(pane));
+			dialoStage.setResizable(false);
+			dialoStage.initOwner(parentSatge);
+			dialoStage.initModality(Modality.WINDOW_MODAL);
+			dialoStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerts.showAlerts("IO Excepition", "Erro loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
